e17249d2a51f515508fdfd4eb7514c7a602d068a
Emerlyy <vadim200339@gmail.com>
2024-01-12 20:36:50 +0200

    added button type for sidebar

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -2,10 +2,12 @@ import styles from "./Button.module.css";
 
 type ButtonColor = "primary" | "secondary" | "destructive";
 type ButtonSize = "l" | "s";
+type ButtonType = "default" | "sidebar";
 
 type Props = {
 	color?: ButtonColor;
 	size?: ButtonSize;
+	type?: ButtonType;
 	className?: string;
 	onClick?: () => void;
 	children: React.ReactNode;
@@ -14,13 +16,14 @@ type Props = {
 const Button = ({
 	color = "primary",
 	size = "s",
+	type = "default",
 	className,
 	onClick,
 	children,
 }: Props) => {
 	const style = `${styles.button} ${styles[size]} ${styles[color]} ${
-		className ? ` ${className}` : ""
-	}`;
+		type === "sidebar" ? ` ${styles.sidebar}` : ""
+	} ${className ? ` ${className}` : ""}`;
 	return (
 		<button onClick={onClick} className={style}>
 			{children}
