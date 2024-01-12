0a1790b8fbab16c982b9aa84ce748a6d1e2ebaaa
Emerlyy <vadim200339@gmail.com>
2024-01-12 20:36:26 +0200

    styling

--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -1,14 +1,17 @@
 import LogoDark from "@/assets/logo-dark.svg";
 import LogoLight from "@/assets/logo-light.svg";
+import { Theme } from "@/types";
+import styles from "./Logo.module.css";
 type Props = {
-	color?: "light" | "dark";
+	color?: Theme;
 	className?: string;
 };
 
 const Logo = ({ color = "dark", className }: Props) => {
 	return (
-		<div className={className}>
+		<div className={`${styles.logo}  ${className ? ` ${className}` : ""}`}>
 			<img
+				className={styles.img}
 				src={color == "dark" ? LogoDark : LogoLight}
 				alt="kanban"
 				width={152}
