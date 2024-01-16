f479aceb496d193b39168e3bab6b09f285432c0a
Emerlyy <vadim200339@gmail.com>
2024-01-16 15:20:08 +0200

    added onClick on Task component

--- src/ui/Task/Task.tsx
+++ src/ui/Task/Task.tsx
@@ -3,18 +3,20 @@ import Text from "../Text/Text";
 import Title from "../Title/Title";
 import styles from "./Task.module.css";
 
-type Props = Pick<TTask, "title" | "subtasks">;
+type Props = Pick<TTask, "title" | "subtasks"> & {
+	onClick?: () => void;
+};
 
-const Task = ({ title, subtasks }: Props) => {
+const Task = ({ title, subtasks, onClick }: Props) => {
 	const completedSubtasks = subtasks.filter(
 		(subtask) => subtask.isCompleted
 	).length;
 	return (
-		<button className={styles.taskBody}>
+		<button className={styles.taskBody} onClick={onClick}>
 			<Title tag="h3" color="primary" size="m" className={styles.taskTitle}>
 				{title}
 			</Title>
-			<Text tag='div' color="gray" weight="700">
+			<Text tag="div" color="gray" weight="700">
 				{completedSubtasks} of {subtasks.length} subtasks
 			</Text>
 		</button>
