ddd1ae3280fb69d5df12512075549b19cd174821
Emerlyy <vadim200339@gmail.com>
2024-01-21 12:56:58 +0200

    created stylized select component using react-select

---
+++ src/ui/Select/Select.tsx
@@ -0,0 +1,65 @@
+import { forwardRef } from "react";
+import CSelect, { ClassNamesConfig, Props, SelectInstance } from "react-select";
+import Text from "../Text/Text";
+import styles from "./Select.module.css";
+
+export type StatusOption = {
+	value: string;
+	label: string;
+};
+
+type SelectProps = {
+	label?: string;
+} & Props<StatusOption, false>;
+
+const classNames: ClassNamesConfig<StatusOption, false> = {
+	control: (state) =>
+		`${styles.control} ${state.isFocused ? ` ${styles.focused}` : ""}`,
+	valueContainer: () => styles.valueContainer,
+	input: () => styles.input,
+	singleValue: () => styles.singleValue,
+	indicatorSeparator: () => styles.separator,
+	indicatorsContainer: () => styles.indicatorsContainer,
+	option: (state) =>
+		`${styles.option} ${state.isSelected ? ` ${styles.selected}` : ""}  ${
+			state.isFocused ? ` ${styles.focused}` : ""
+		}`,
+	menu: () => styles.menu,
+	menuList: () => styles.menuList,
+};
+
+const Select = forwardRef<SelectInstance<StatusOption, false>, SelectProps>(
+	({ label, ...props }, ref) => {
+		return (
+			<div className={styles.selectWrapper}>
+				{label && (
+					<Text tag="span" size="m" color="label" weight="700">
+						{label}
+					</Text>
+				)}
+				<CSelect
+					ref={ref}
+					classNames={classNames}
+					components={{
+						DropdownIndicator: DownChevron,
+					}}
+					isSearchable={false}
+					{...props}
+				/>
+			</div>
+		);
+	}
+);
+
+const DownChevron = () => (
+	<svg
+		width="10"
+		height="7"
+		xmlns="http://www.w3.org/2000/svg"
+		className={styles.dropdownIcon}
+	>
+		<path stroke="#635FC7" strokeWidth="2" fill="none" d="m1 1 4 4 4-4" />
+	</svg>
+);
+
+export default Select;
