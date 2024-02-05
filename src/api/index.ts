83fd5e9962821f758022bfe56814debe460feba6
Emerlyy <vadim200339@gmail.com>
2024-02-05 11:41:39 +0200

    randomized api response time

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,10 +5,14 @@ export type Api = {
 	getAllBoards: () => Promise<Board[]>;
 };
 
+const getRandom = (min: number, max: number) => {
+	return Math.floor(Math.random() * (max - min) + min);
+};
+
 const client: Api = {
 	getAllBoards: async (): Promise<Board[]> => {
 		return await new Promise((resolve) =>
-			setTimeout(() => resolve(data.boards as Board[]), 3000)
+			setTimeout(() => resolve(data.boards as Board[]), getRandom(300, 1500))
 		);
 	},
 };
