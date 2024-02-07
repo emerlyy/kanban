022bd73912e0fe34dae6f817472942cbdb6fe391
Emerlyy <vadim200339@gmail.com>
2024-02-07 15:18:22 +0200

    added changing color depending on the theme in the logo component

--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -1,25 +1,26 @@
 import LogoDark from "@/assets/logo-dark.svg";
 import LogoLight from "@/assets/logo-light.svg";
 import LogoSmall from "@/assets/logo-mobile.svg";
+import { useTheme } from "@/context/ThemeContext";
 import useMediaQuery from "@/hooks/useMediaQuery";
-import { Theme } from "@/types";
 import styles from "./Logo.module.css";
+
 type Props = {
-	color?: Theme;
 	className?: string;
 };
 
-const Logo = ({ color = "dark", className }: Props) => {
+const Logo = ({ className }: Props) => {
+	const { theme } = useTheme();
 	const isAboveSmallScreens = useMediaQuery("(min-width:601px)");
 
 	const logoSrc = isAboveSmallScreens
-		? color === "dark"
+		? theme === "light"
 			? LogoDark
 			: LogoLight
 		: LogoSmall;
 
 	return (
-		<div className={`${styles.logo}  ${className ? ` ${className}` : ""}`}>
+		<div className={`${styles.logo}${className ? ` ${className}` : ""}`}>
 			<img className={styles.img} src={logoSrc} alt="kanban" />
 		</div>
 	);
